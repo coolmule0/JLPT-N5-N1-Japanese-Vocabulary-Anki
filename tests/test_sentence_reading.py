9c1ba77a60e88aba593977f6d48bee92dae3ff95
coolmule0 <coolmule0@protonmail.com>
2026-07-31 11:21:36 +0100

    test for various terms that have been causing issues before. (Would hate for regression in these)

--- tests/test_sentence_reading.py
+++ tests/test_sentence_reading.py
@@ -25,6 +25,13 @@ from sources.sentences.sentence_matcher import SentenceMatcher
 # ── Edit these terms to test ──────────────────────────────────────────
 TEST_CASES = [
 	("越す", "また<mark>お 越[こ]し</mark>ください。"),
+    ("積もる", "塵[ちり]も<mark>積[つ]もれば</mark> 山[やま]となる。"),
+    ("学", "<mark>学[がく]</mark>のある 人[ひと]はとかく 無知[むち]な 人[ひと]を 軽蔑[けいべつ]する。"),
+    ("食べる", "僕[ぼく]は 脚本家[きゃくほんか]で<mark>食[た]べて</mark>いく 決心[けっしん]をした。"),
+    ("ビル", "あの<mark>ビル</mark>のとなりです。"), #uses the noun rather than proper name "Bill"
+    ("郵便局", "あの～<mark>郵便局[ゆうびんきょく]</mark>はどちらでしょうか。"), # matches fully all 3 kanji where 2 kanjis +1 could also make a valid sentence
+    ("喧嘩", "ケンと<mark>けんか</mark>したのか。"), # picks up proper nouns like ケン which aren't in the index list
+    ("小さな", "その 犬[いぬ]は<mark>小[ちい]さな</mark> 男[おとこ]の 子[こ]に 向[む]かって 唸[うな]った。"), # furigana only displayed over the necessary kanji part, and cuts hiragana parts from the reading
 ]
 # ──────────────────────────────────────────────────────────────────────
 
